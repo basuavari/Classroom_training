78d2668281f797f3e746fbe315c0e1f250d97aa6
basuavari <96293061+basuavari@users.noreply.github.com>
2021-12-20 12:01:56 +0530

    code uploaded

---
+++ Array_programs/duplicate replace with zero.c
@@ -0,0 +1,40 @@
+ #include<stdio.h>
+int main()
+{
+  int n, list[100],i,j,k;
+  scanf("%d", &n);
+  for(i=0; i<n; i++)
+    scanf("%d", &list[i]);
+    for(i=0; i<n; i++)
+    {
+      for(j=i+1; j<n; )
+      {
+        if(list[j]==list[i])
+        {
+        for(k=j; k<n; k++)
+     list[k]=list[k+1];
+      // list[j]=0;
+          n--;
+        }
+         else
+         {
+           j++;
+         }
+      }
+    }
+for(i=0; i<n; i++)
+  printf("%d\n", list[i]);
+ return 0;
+}
+
+
+
+
+
+Test cases Sample Input :
+
+(Length:) 5 (Values:) 2 3 4 3 6
+
+Sample Output:
+
+2  3  4  0  6
